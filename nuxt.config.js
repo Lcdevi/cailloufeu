2b34baad3b5b652c4fc1b14c98ad0bf2bead1326
Lcdevi <caillauxlaura@gmail.com>
2021-09-23 13:23:49 +0200

    systemvar true

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    new Dotenv({
+      systemvars: true
+    })
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
